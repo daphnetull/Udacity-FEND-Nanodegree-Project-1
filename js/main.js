fbcf649cec9c601ad2cf32fb29a334ffa3b37ecb
daphnetull <palebluebeyond@icloud.com>
2018-05-10 01:07:18 -0400

    first submission

--- js/main.js
+++ js/main.js
@@ -1,27 +1,10 @@
 $(document).ready(function(){
 	
 	var $menuIcon = $('.menu-icon');
-	var $mobileNavMenu = $('.mobile-nav');
+	var $mobileNavMenu = $('.main-nav');
 
 	$menuIcon.click(function(){
 		$mobileNavMenu.toggleClass('open');
 	});
-
-
-
-
-
+  
 });
-
-/*      var menu = document.querySelector('#menu');
-      var main = document.querySelector('main');
-      var drawer = document.querySelector('#drawer');
-
-      menu.addEventListener('click', function(e) {
-        drawer.classList.toggle('open');
-        e.stopPropagation();
-      });
-
-      main.addEventListener('click', function() {
-        drawer.classList.remove('open');
-      });*/
